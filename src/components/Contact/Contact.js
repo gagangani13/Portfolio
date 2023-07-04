9f46b2e812f49f6630404abf308602aa060f82d9
gagangani13 <gagangani17@gmail.com>
2023-07-05 01:32:32 +0530

    Update

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Contact.css";
-import gmail from './gmail.jpg'
-import linkedIn from './linkedIn.jpg'
+import gmail from "./gmail.jpg";
+import linkedIn from "./linkedIn.jpg";
 import { Element } from "react-scroll";
 import { FaArrowUp } from "react-icons/fa";
 
@@ -14,22 +14,41 @@ const Contact = () => {
   };
   return (
     <>
-    <Element name="Contact" className="formLayout">
-      <lottie-player
-        src="https://assets10.lottiefiles.com/packages/lf20_eroqjb7w.json"
-        background="transparent"
-        speed="1"
-        loop
-        autoplay
-        id='contactAnim'
-      ></lottie-player>
-      <h2>Contact me</h2>
-      <div className="sm">
-        <a href="mailto:gagangani17@gmail.com" className="animate__animated animate__pulse animate__infinite	"><img src={gmail} alt='gmail' className="gmail"/></a>
-        <a href="https://www.linkedin.com/in/gagan-h-n/" className="animate__animated animate__pulse animate__infinite	" target="blank"><img src={linkedIn} alt='linkedIn' className="linkedIn"/></a>
-      </div>
-    </Element>
-    <button className="topBtn" type="button" onClick={scrollToTop}><FaArrowUp/></button>
+      <Element name="Contact" className="formLayout">
+        <h2 className="wave-animation">
+          Thank you for visiting my Portfolio
+          <span role="img" aria-label="smiley" className="word">
+            🙂
+          </span>
+        </h2>
+        <lottie-player
+          src="https://assets10.lottiefiles.com/packages/lf20_eroqjb7w.json"
+          background="transparent"
+          speed="1"
+          loop
+          autoplay
+          id="contactAnim"
+        ></lottie-player>
+        <h2>Contact me</h2>
+        <div className="sm">
+          <a
+            href="mailto:gagangani17@gmail.com"
+            className="animate__animated animate__pulse animate__infinite	"
+          >
+            <img src={gmail} alt="gmail" className="gmail" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/gagan-h-n/"
+            className="animate__animated animate__pulse animate__infinite	"
+            target="blank"
+          >
+            <img src={linkedIn} alt="linkedIn" className="linkedIn" />
+          </a>
+          </div>
+      </Element>
+      <button className="topBtn" type="button" onClick={scrollToTop}>
+        <FaArrowUp />
+      </button>
     </>
   );
 };
